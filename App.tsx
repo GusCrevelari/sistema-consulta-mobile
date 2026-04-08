48e0f9eb69405033e185ab80d9083391c9555352
GusCrevelari <gustavocrevelari@hotmail.com>
2026-04-08 19:49:59 -0300

    fixed import paths in App.tsx;

--- App.tsx
+++ App.tsx
@@ -1,50 +1,11 @@
-import React, { useEffect } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
 import Home from "./src/screens/HomeScreen";
 import Admin from "./src/screens/Admin";
 import CadastroPaciente from "./src/screens/CadastroPaciente";
-import Agendamento from "../sistema-consulta-mobile/src/screens/Agendamento";
-import { inicializarDados } from "../sistema-consulta-mobile/src/service/storage";
+import Agendamento from "./src/screens/Agendamento";
+import Navigation from "./src/navigation";
 
 
-const Stack = createNativeStackNavigator();
 export default function App() {
-  // Inicializa dados quando o app carrega
-  useEffect(() => {
-    inicializarDados();
-  }, []);
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"  // Começa no login
-        screenOptions={{
-          headerStyle: { backgroundColor: "#79059C" },
-          headerTintColor: "#fff",
-          headerTitleStyle: { fontWeight: "bold" },
-        }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={CadastroPaciente}
-          options={{ title: "Entrar / Cadastrar", headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: "Minhas Consultas" }}
-        />
-        <Stack.Screen
-          name="Agendamento"
-          component={Agendamento}
-          options={{ title: "Agendar Consulta" }}
-        />
-        <Stack.Screen
-          name="Admin"
-          component={Admin}
-          options={{ title: "Painel Administrativo" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
