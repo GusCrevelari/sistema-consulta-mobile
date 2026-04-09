d8683f788641e634c77b4bc375fc9fef97d40b4b
GusCrevelari <gustavocrevelari@hotmail.com>
2026-04-08 23:17:09 -0300

    fix: App.tsx fixed;

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
+/**
+ * App.tsx - Ponto de Entrada do Aplicativo
+ * Configuração principal com navegação
+ */
+
 import React from "react";
-import Home from "./src/screens/HomeScreen";
-import Admin from "./src/screens/Admin";
-import CadastroPaciente from "./src/screens/CadastroPaciente";
-import Agendamento from "./src/screens/Agendamento";
+import { StatusBar } from "expo-status-bar";
 import Navigation from "./src/navigation";
 
-
 export default function App() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <StatusBar style="light" />
+    </>
+  );
 }
